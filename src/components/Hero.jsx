2c354ad689c0b640a8d5aaac0423ecbc07c18685
Kayla Hunt <huntkay1@msu.edu>
2024-08-24 20:09:33 -0400

    Style and add cta content
    Added styling to both nav and hero cta.
    Added cta content.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,11 @@ function Hero() {
         <section className='hero'>
             <div className='hero-one'></div>
             <div className='hero-two'></div>
-            <div className='hero-cta'></div>
+            <div className='hero-cta'>
+                <h2>Spring Into Fall</h2>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Esse, tempora. Molestiae possimus sint maxime amet, quaerat animi quos necessitatibus fuga odit rerum sapiente!</p>
+                <button>Shop Now</button>
+            </div>
         </section>
     )
 }
