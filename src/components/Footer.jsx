cc3700b56ca16574da509113a545c60b949cb400
Kayla Hunt <huntkay1@msu.edu>
2024-08-29 19:40:00 -0400

    Create footer, add content

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,36 @@
+import '../styles/Footer.css'
+
 function Footer() {
     return(
         <footer>
-
+            <div className='list-block'>
+                <h3>Products</h3>
+                <ul className='footer-list'>
+                    <li>Tropical Plants</li>
+                    <li>Cacti & Succulents</li>
+                    <li>Foliage</li>
+                    <li>Pots & Accessories</li>
+                </ul>
+            </div>
+            <div className='list-block'>
+                <h3>Other Pages</h3>
+                <ul className='footer-list'>
+                    <li>About</li>
+                    <li>Contact</li>
+                    <li>Terms & Conditions</li>
+                    <li>Shipping Policy</li>
+                    <li>Return Policy</li>
+                </ul>
+            </div>
+            <div className='list-block'>
+                <h3>Socials</h3>
+                <ul className='footer-list'>
+                    <li>Facebook</li>
+                    <li>Instagram</li>
+                    <li>X</li>
+                    <li>TikTok</li>
+                </ul>
+            </div>
         </footer>
     )
 }
