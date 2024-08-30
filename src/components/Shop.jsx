e98ae4f975a3a3b0f26446feb6f9e4f8e8a50249
Kayla Hunt <huntkay1@msu.edu>
2024-08-29 20:31:01 -0400

    Add shop page content
    Added side nav and card container layout for
    product cards.
    
    Mocked out product card layout. Will be adding
    real data from an API for the cards.

--- src/components/Shop.jsx
+++ src/components/Shop.jsx
@@ -1,7 +1,68 @@
+import Navbar from './Navbar';
+import Footer from './Footer'
+import placeholder from '../assets/hero-3.jpg'
+import '../styles/Shop.css'
+
 function Shop() {
     return(
         <>
-            
+            <Navbar />
+            <section className='shop'>
+                <div className='shop-nav'>
+                    <ul>
+                        <li>Tropical Plants</li>
+                        <li>Cacti & Succulents</li>
+                        <li>Foliage</li>
+                        <li>Pots & Accessories</li>
+                    </ul>
+                </div>
+                <div className='card-container'>
+                    <div className='product-card'>
+                        <img src={placeholder} width='100%'></img>
+                        <div className='product-details'>
+                            <h4>Plant Name</h4>
+                            <p>$20.00</p>
+                        </div>
+                    </div>
+                    <div className='product-card'>
+                        <img src={placeholder} width='100%'></img>
+                        <div className='product-details'>
+                            <h4>Plant Name</h4>
+                            <p>$20.00</p>
+                        </div>
+                    </div>
+                    <div className='product-card'>
+                        <img src={placeholder} width='100%'></img>
+                        <div className='product-details'>
+                            <h4>Plant Name</h4>
+                            <p>$20.00</p>
+                        </div>
+                    </div>
+                    <div className='product-card'>
+                        <img src={placeholder} width='100%'></img>
+                        <div className='product-details'>
+                            <h4>Plant Name</h4>
+                            <p>$20.00</p>
+                        </div>
+                    </div>
+                    <div className='product-card'>
+                        <img src={placeholder} width='100%'></img>
+                        <div className='product-details'>
+                            <h4>Plant Name</h4>
+                            <p>$20.00</p>
+                        </div>
+                    </div>
+                    <div className='product-card'>
+                        <img src={placeholder} width='100%'></img>
+                        <div className='product-details'>
+                            <h4>Plant Name</h4>
+                            <p>$20.00</p>
+                        </div>
+                    </div>
+                </div>
+                
+            </section>
+            <Footer />
         </>
     )
 }
