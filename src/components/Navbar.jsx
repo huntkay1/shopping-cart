ad2a57d375c17962718c765b43e5efc94bab5b0a
Kayla Hunt <huntkay1@msu.edu>
2024-08-29 20:41:16 -0400

    Add home link & update style

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,6 +9,9 @@ function Navbar() {
             <nav>
                 <ul>
                     <li>
+                        <Link to='/'>Home</Link>
+                    </li>
+                    <li>
                         <Link to='/shop'>Shop</Link>
                     </li>
                     <li>
