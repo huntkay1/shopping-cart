c0e0f82df201943dfbd7bf02189cc400cc253435
Kayla Hunt <huntkay1@msu.edu>
2024-09-07 18:55:45 -0400

    Remove signup section img

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -4,17 +4,13 @@ import arrowIcon from '../assets/arrow-icon.svg'
 function SignUp() {
     return(
         <section className='signup-section'>
-            <div className='signup-img'></div>
-            <div>
-                <h2>Sign up for our newsletter</h2>
-                <div className='input-container'>
-                    <input type='email' placeholder='Email'></input>
-                    <button type='submit'>
-                        <p>Subscribe</p>
-                        <img src={arrowIcon}></img>
-                    </button>
-                </div>
-
+            <h2>Sign up for our newsletter</h2>
+            <div className='input-container'>
+                <input type='email' placeholder='Email'></input>
+                <button type='submit'>
+                    <p>Subscribe</p>
+                    <img src={arrowIcon}></img>
+                </button>
             </div>
         </section>
     )
