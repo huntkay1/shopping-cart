2aeb1e689d86bbeedb70fd5c7d920c9e6744cb0c
Kayla Hunt <huntkay1@msu.edu>
2024-09-09 17:31:50 -0400

    Find selected product data
    Added logic to the ProductPage to locate the
    correct product to display the information on the
    page

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,8 +1,30 @@
 import { useParams } from 'react-router-dom'
+import { getProductData } from './productData';
+import { useState, useEffect } from 'react'
 
 function ProductPage() {
-    const id  = useParams();
-    console.log(plantData)
+    const { id }  = useParams();
+    const productData = getProductData();
+    const [selectedProduct, setSelectedProduct] = useState(null)
+
+    useEffect(() => {
+        if(productData) {
+            setSelectedProduct(productData.find(product => product.product_id == id))
+        }
+    }, [productData]);
+    
+    return (
+
+            <div className='product-page'>
+                {selectedProduct && 
+                    
+                    <h3>{selectedProduct.product_name}</h3>
+                }
+                
+            </div>
+        
+
+    )
 }
 
 export default ProductPage
