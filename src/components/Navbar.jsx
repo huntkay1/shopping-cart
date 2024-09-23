4ddcd94d797763745c971ec231a191aeca981786
Kayla Hunt <huntkay1@msu.edu>
2024-09-23 16:36:17 -0400

    Make navhover permanent
    The line that is formed under the nav links when
    hovered are now made permanent when the link
    is active. To do this, I changed the Link to
    NavLink

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
 import shoppingCart from '../assets/cart-icon.svg'
 import '../styles/Navbar.css'
-import { Link } from 'react-router-dom'
+import { NavLink } from 'react-router-dom'
 import { useCart } from './CartContext'
 
 function Navbar() {
 
     const { cartQuantity } = useCart();
+
+    const setActive = ({ isActive }) =>
+        isActive ? "active-link" : "";
 
 
     return (
@@ -13,23 +16,23 @@ function Navbar() {
             <nav>
                 <ul>
                     <li>
-                        <Link to='/'>Home</Link>
+                        <NavLink to='/' className={setActive}>Home</NavLink>
                     </li>
                     <li>
-                        <Link to='/shop'>Shop</Link>
+                        <NavLink to='/shop' className={setActive}>Shop</NavLink>
                     </li>
                     <li>
-                        <Link to='/about'>About</Link>
+                        <NavLink to='/about' className={setActive}>About</NavLink>
                     </li>
                 </ul> 
             </nav>
             <div className='logo'>  <h1>Leafy Greens</h1></div>
             
             <div className='cart'>
-                <Link to='/cart'>
+                <NavLink to='/cart'>
                     <img src={shoppingCart} className='cart-icon'></img>
                     <span id='cart-badge' className={cartQuantity > 0 ? 'active' : ''}>{cartQuantity}</span>
-                </Link></div>
+                </NavLink></div>
         </section>
     )
 }
