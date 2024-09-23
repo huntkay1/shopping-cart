3bada39a8a33038215e7dc8e5db4704c46bb889b
Kayla Hunt <huntkay1@msu.edu>
2024-09-23 17:49:52 -0400

    Move to supabase
    Moved API data from MySQL to Supabase. I updated
    the productData.js file to reflect this - now
    calling the supabase api instead of my custom
    api.

--- src/components/productData.js
+++ src/components/productData.js
@@ -1,4 +1,9 @@
 import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js'
+const supabaseUrl = 'https://sdbbyfkkilxegwnepgdz.supabase.co'
+const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InNkYmJ5ZmtraWx4ZWd3bmVwZ2R6Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MjcxMjU0NjgsImV4cCI6MjA0MjcwMTQ2OH0.S6NHVLMV7ar_GL4kVmZRtmTWfXwmDnowgp_t7th3YR8'
+const supabase = createClient(supabaseUrl, supabaseKey);
+
 
 export function getProductData() {
     const [plantData, setPlantData] = useState(null);
@@ -6,8 +11,15 @@ export function getProductData() {
     useEffect(() => {
         const fetchData = async () => {
             try {
-                const response = await fetch('http://localhost:3000/products', { mode: 'cors' });
-                const data = await response.json();
+                // Fetch data from Supabase
+                const { data, error } = await supabase
+                    .from('plant-shop-data')  // Use the correct table name
+                    .select('*');
+
+                if (error) {
+                    throw error;
+                }
+
                 setPlantData(data);
             } catch (error) {
                 console.error('Error fetching plant data:', error);
